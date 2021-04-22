617c0f522d102057a64d8b4e6e87525760a642da
Philipp Thalhammer <57066673+Phlipinator@users.noreply.github.com>
2021-04-22 21:04:26 +0200

    The CameraFollow Script has been rewritten completly and now works smoothly

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,7 +8,7 @@ public class CameraFollow : MonoBehaviour
     public GameObject startScreen;
     public GameObject Boundary;
     public float moveAmount = 0.01f;
-    public float edgeSize = 10f;
+    public float edgeSize = 0.1f;
     public float cameraMoveSpeed = 1f;
 
     public bool enableScrolling = true;
@@ -24,30 +24,35 @@ public class CameraFollow : MonoBehaviour
     private float width;
     private float zoom;
 
+    private bool activateMouseMovement = false;
+
     private float aspect;
 
     private Camera cam;
 
     void Start()
     {
+        cam = Camera.main;
+
+        zoom = cam.orthographicSize;
+
+        aspect = cam.aspect;
+
+
         //initialize the starting position of the camera
-        Vector3 cameraFollowPosition = startScreen.transform.position;
+        Vector2 cameraFollowPosition = new Vector2(startScreen.transform.position.x, startScreen.transform.position.y);
+        cam.transform.position = new Vector3(cameraFollowPosition.x, cameraFollowPosition.y, -10f);
+        
 
         // Gets the height and with of the Background Image
         width = Boundary.GetComponent<SpriteRenderer>().bounds.size.x;
         height = Boundary.GetComponent<SpriteRenderer>().bounds.size.y;
 
-        cam = Camera.main;
-        zoom = cam.orthographicSize;
-
-        aspect = cam.aspect;
-
-        cam.transform.position = new Vector3(startScreen.transform.position.x, startScreen.transform.position.y, -10f);
-
     }
 
     void Update()
     {
+
         float camWidth = zoom * aspect * 2;
         float camHeight = zoom * 2;
 
@@ -55,85 +60,86 @@ public class CameraFollow : MonoBehaviour
 
         boundaryX = (width / 2) - (camWidth / 2) ;
         boundaryY = (height / 2) - (camHeight / 2);
-        
+
         // Debug.LogFormat("BX: {0} BY: {1} CX: {2} CY: {3}", boundaryX, boundaryY, cameraFollowPosition.x, cameraFollowPosition.y);
 
-        // move to the right
-        if ((Input.mousePosition.x > Screen.width - edgeSize) && cameraFollowPosition.x < boundaryX)
-        {
-            cameraFollowPosition.x += dynamicMoveAmount;
-        }
+        Vector2 mouseUnitPos = new Vector2(Input.mousePosition.x / Screen.width * 2 - 1, Input.mousePosition.y / Screen.height * 2 - 1);
 
-        // move to the left
-        if ((Input.mousePosition.x < edgeSize) && cameraFollowPosition.x > -boundaryX)
-        {
-            cameraFollowPosition.x -= dynamicMoveAmount;
-        }
+        Vector2 camPos2D = new Vector2(cam.transform.position.x, cam.transform.position.y);
 
-        // move up
-        if ((Input.mousePosition.y > Screen.height - edgeSize) && cameraFollowPosition.y < boundaryY)
-        {
-            cameraFollowPosition.y += dynamicMoveAmount;
-        }
-
-        // move down
-        if ((Input.mousePosition.y < edgeSize) && cameraFollowPosition.y > -boundaryY)
-        {
-            cameraFollowPosition.y -= dynamicMoveAmount;
-        }
+        Vector2 mouseWorldPos = mouseUnitPos + camPos2D;
 
 
         // correct cam placement RIGHT
-        if (cameraFollowPosition.x > boundaryX)
+        if (mouseWorldPos.x > boundaryX)
         {
-            cameraFollowPosition.x = boundaryX;
+            mouseWorldPos.x = boundaryX;
         }
 
         // correct cam placment LEFT
-        if (cameraFollowPosition.x < -boundaryX)
+        if (mouseWorldPos.x < -boundaryX)
         {
-            cameraFollowPosition.x = -boundaryX;
+            mouseWorldPos.x = -boundaryX;
         }
 
         // correct cam placement UP
-        if (cameraFollowPosition.y > boundaryY)
+        if (mouseWorldPos.y > boundaryY)
         {
-            cameraFollowPosition.y = boundaryY;
+            mouseWorldPos.y = boundaryY;
         }
 
         // correct cam placement DOWN
-        if (cameraFollowPosition.y < -boundaryY)
+        if (mouseWorldPos.y < -boundaryY)
         {
-            cameraFollowPosition.y = -boundaryY;
+            mouseWorldPos.y = -boundaryY;
+        }
+
+
+        Vector2 cameraMoveDir = (mouseWorldPos - camPos2D);
+
+
+        float magnitude = mouseUnitPos.magnitude;
+
+
+        float deadZoneRadius = 0.3f;
+
+
+        if (magnitude > 1f)
+        {
+            magnitude = 1f;
+        }
+
+
+        float moveFactor;
+
+        if (magnitude < deadZoneRadius)
+        {
+            moveFactor = 0f;
+
+        } else
+        {
+            float a = (magnitude - deadZoneRadius) / (1f - deadZoneRadius);
+
+            moveFactor = a * a;
+        }
+        
+
+        if (activateMouseMovement)
+        {
+            Vector3 movement = cameraMoveDir * cameraMoveSpeed * Time.deltaTime * moveFactor;
+
+        
+            cam.transform.position += movement;
+
+        } else if (!activateMouseMovement && mouseUnitPos.magnitude < deadZoneRadius)
+        {
+            activateMouseMovement = true;
         }
 
 
 
-        //Sets a static value for z, because we dont need it in 2D
-        cameraFollowPosition.z = transform.position.z;
 
-        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
-        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
-
-        if (distance > 0)
-        {
-            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
-
-            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
-
-            if (distanceAfterMoving > distance)
-            {
-                // Overshot the target
-
-                newCameraPosition = cameraFollowPosition;
-            }
-
-            cam.transform.position = newCameraPosition;
-
-        }
-
-
-        if (enableScrolling == true)
+        if (enableScrolling)
         {
             float zoomChangeAmount = 800f;
 
